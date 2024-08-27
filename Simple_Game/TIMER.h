c220c0af6db0ef77f4cbb8aa321b82e34c7cd32d
Ahmed Mogoda <ahmedragabmogouda@gmail.com>
2024-08-27 16:17:11 +0300

    Update TIMER.h

--- Simple_Game/TIMER.h
+++ Simple_Game/TIMER.h
@@ -16,30 +16,8 @@ void TIMER0_normal_init_with_interrupt(void);
 
 void TIMER0_CTC_init_with_interrupt(void);
 
-void TIMER0_CTC_wave_nonpwm(void);
-
-void TIMER0_fast_pwm_wave(void);
-
-void TIMER0_phase_correct_pwm_wave(void);
-
 void TIMER2_normal_init_with_interrupt(void);
 
 void TIMER2_CTC_init_with_interrupt(void);
-
-void TIMER2_CTC_wave_nonpwm(void);
-
-void TIMER2_fast_pwm_wave(void);
-
-void TIMER2_phase_correct_pwm_wave(void);
-
-void TIMER1_fast_pwm_8bit_wave(void);
-
-void TIMER1_ICU_init(void);
-
-unsigned short TIMER1_ICU_raising(void);
-
-unsigned short TIMER1_ICU_failing(void);
-
-void TIMER1_ICU_init_with_interrupt(void);
 
 #endif /* TIMER_H_ */
